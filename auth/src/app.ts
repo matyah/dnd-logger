f02cf11aa295f96993d6ddbaa443bedf0c92e2b8
Haytam Jebbari <hjebbari@lefigaro.fr>
2022-07-19 17:02:04 +0200

    ci: adding logs

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,9 @@
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import express from "express";
+import { transports, format } from "winston";
+import { LoggerOptions, logger } from "express-winston";
+import debug from "debug";
 import "express-async-errors";
 import { NotFoundError } from "./common/errors/not-found-error";
 import { errorHandler } from "./common/middleware/error-handler";
@@ -11,6 +14,8 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 
 const app = express();
+const debugLog: debug.IDebugger = debug("app");
+
 app.set("trust proxy", true);
 app.use(json());
 app.use(
@@ -21,6 +26,23 @@ app.use(
   })
 );
 
+// Log
+const loggerOptions: LoggerOptions = {
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.json(),
+    format.prettyPrint(),
+    format.colorize({ all: true })
+  ),
+};
+
+if (!process.env.DEBUG) {
+  loggerOptions.meta = false;
+}
+
+app.use(logger(loggerOptions));
+
+// Routes
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
@@ -30,6 +52,7 @@ app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
+// Middlewares
 app.use(errorHandler);
 
 export { app };
